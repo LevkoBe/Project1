e34b591d0f18112742177836d968592691d49216
Levko <lbeniakh@kse.org.ua>
2023-09-06 11:18:43 +0300

    Commands 0, 1, 2, and 10 work somehow

--- FirstFile.c
+++ FirstFile.c
@@ -20,12 +20,48 @@ int main()
 		else if (command == 1)
 		{
 			printf("\nEnter the text you want to add: \n");
-			scanf_s("%99s", &additional_text, sizeof(additional_text));
+			scanf_s(" %99[^\n]", &additional_text, sizeof(additional_text));
 			strcat(text, additional_text);
 		}
 		else if (command == 2)
 		{
 			strcat(text, "\n");
+			printf("\nNew line was started.\n");
+			command = 10;
+		}
+		else if (command == 3) {
+
+		}
+		else if (command == 4) {
+
+		}
+		else if (command == 5) {
+
+		}
+		else if (command == 6) {
+
+		}
+		else if (command == 7) {
+
+		}
+		else if (command == 8) {
+
+		}
+		else if (command == 9) {
+
+		}
+		else {
+			printf("\nCommand 0 is for exit;\n");
+			printf("Command 1 is for adding some text;\n");
+			printf("Command 2 is for adding a new line;\n");
+			printf("Command 3 is for ;\n");
+			printf("Command 4 is for ;\n");
+			printf("Command 5 is for ;\n");
+			printf("Command 6 is for ;\n");
+			printf("Command 7 is for ;\n");
+			printf("Command 8 is for ;\n");
+			printf("Command 9 is for ;\n");
+			printf("Command 10 is for instructions.\n");
 		}
 		printf("\nThe text is \"%s\" now.\n", text);
 	}
