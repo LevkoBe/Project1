03f699e12917f815671ab21a396b1936d83edf1d
Levko <lbeniakh@kse.org.ua>
2023-09-06 09:53:12 +0300

    Reads word by word

--- FirstFile.c
+++ FirstFile.c
@@ -2,6 +2,34 @@
 
 int main()
 {
+	char text[] = "";
+	char additional_text[100];
+	int command = 0;
+
+	while (1)
+	{
+		printf("\nChoose the command:\n");
+
+		printf("(from 0 to 10, where 10 is instructions): ");
+		scanf_s("%d", &command);
+
+		if (command == 0)
+		{
+			break;
+		}
+		else if (command == 1)
+		{
+			printf("\nEnter the text you want to add: \n");
+			scanf_s("%99s", &additional_text, sizeof(additional_text));
+			strcat(text, additional_text);
+		}
+		else if (command == 2)
+		{
+			strcat(text, "\n");
+		}
+		printf("\nThe text is \"%s\" now.\n", text);
+	}
+
 
 	return 0;
 }
