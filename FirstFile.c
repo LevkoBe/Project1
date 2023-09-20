a33a1b0e37aa76834a1035e8002272b78d96fc71
LevkoBe <lbeniakh@kse.org.ua>
2023-09-20 09:34:45 +0300

    All 8 commands work.

--- FirstFile.c
+++ FirstFile.c
@@ -3,7 +3,19 @@
 
 char text[100] = "";
 
-void insert_line(int line, int column, int text_length, int inserting, const char* additional_text) {
+void insert_line() {
+    int line;
+    int column;
+    char additional_text[100];
+    printf_s("Line: ");
+    scanf_s("%d", &line);
+    printf_s("Column: ");
+    scanf_s("%d", &column);
+    printf("Enter text to insert: ");
+    scanf_s(" %99[^\n]", additional_text, sizeof(additional_text));
+    int text_length = strlen(text);
+    int inserting = strlen(additional_text);
+
     int lines = 0;
     int position = 0;
     char result[100] = "";
@@ -62,6 +74,40 @@ void search_text(char* needed) {
     }
 }
 
+void save_to_file() {
+    printf("Enter a name of the file you want to use for saving the text: ");
+    char filename[20];
+    scanf_s(" %19[^\n]", filename, sizeof(filename));
+
+    FILE* file;
+    if (fopen_s(&file, filename, "w") != 0) {
+        printf("Error opening file");
+    }
+    else {
+        fputs(text, file);
+        fclose(file);
+    }
+}
+
+void load_from_file() {
+    printf("Enter a name of the file you want to use for loading the text: ");
+    char filename[20];
+    scanf_s(" %19[^\n]", filename, sizeof(filename));
+
+    FILE* file;
+    char mystring[100];
+    if (fopen_s(&file, filename, "r") != 0) {
+        printf("Error opening file");
+    }
+    else {
+        strcpy(text, "");
+        while (fgets(mystring, sizeof(mystring), file) != NULL) {
+            strcat(text, mystring);
+        }
+        fclose(file);
+    }
+}
+
 int main() {
 
     char additional_text[100];
@@ -70,7 +116,7 @@ int main() {
     while (1) {
         printf("\nChoose the command:\n");
         command = 10;
-        printf("(from 0 to 10, where 10 is instructions): ");
+        printf("(from -1 to 8, where 8 is instructions): ");
         scanf_s("%d", &command);
 
         if (command == 0) {
@@ -90,77 +136,34 @@ int main() {
             printf("\nNew line was started.\n");
         }
         else if (command == 3) {
-            printf("Enter a name of the file you want to use for saving the text: ");
-            char filename[20];
-            scanf_s(" %19[^\n]", filename, sizeof(filename));
-
-            FILE* file;
-            if (fopen_s(&file, filename, "w") != 0) {
-                printf("Error opening file");
-            }
-            else {
-                fputs(text, file);
-                fclose(file);
-            }
+            save_to_file();
         }
         else if (command == 4) {
-            printf("Enter a name of the file you want to use for loading the text: ");
-            char filename[20];
-            scanf_s(" %19[^\n]", filename, sizeof(filename));
-
-            FILE* file;
-            char mystring[100];
-            if (fopen_s(&file, filename, "r") != 0) {
-                printf("Error opening file");
-            }
-            else {
-                strcpy(text, "");
-                while (fgets(mystring, sizeof(mystring), file) != NULL) {
-                    strcat(text, mystring);
-                }
-                fclose(file);
-            }
+            load_from_file();
         }
 
         else if (command == 5) {
             printf("\nThe text is \"%s\" now.\n", text);
         }
         else if (command == 6) {
-            int line;
-            int column;
-            printf_s("Line: ");
-            scanf_s("%d", &line);
-            printf_s("Column: ");
-            scanf_s("%d", &column);
-            printf("Enter text to insert: ");
-            scanf_s(" %99[^\n]", additional_text, sizeof(additional_text));
-            int text_length = strlen(text);
-            int inserting = strlen(additional_text);
-            insert_line(line, column, text_length, inserting, additional_text);
+            insert_line();
         }
         else if (command == 7) {
             printf("Enter text to search: ");
             scanf_s(" %99[^\n]", additional_text, sizeof(additional_text));
             search_text(additional_text);
         }
-        else if (command == 8) {
-
-        }
-        else if (command == 9) {
-
-        }
         else {
-            printf("\nCommand 0 is for exit;\n");
+            printf("\nCommand -1 is for clearing all the text;\n");
+            printf("Command 0 is for exit;\n");
             printf("Command 1 is for adding some text;\n");
             printf("Command 2 is for adding a new line;\n");
             printf("Command 3 is for saving the text in a file;\n");
             printf("Command 4 is for loading some text from a file;\n");
             printf("Command 5 is for printing the text;\n");
-            printf("Command 6 is for ;\n");
-            printf("Command 7 is for ;\n");
-            printf("Command 8 is for ;\n");
-            printf("Command 9 is for ;\n");
-            printf("Command 10 is for instructions.\n");
+            printf("Command 6 is for inserting text;\n");
+            printf("Command 7 is for searching some text;\n");
+            printf("Command 8 is for instructions.\n");
         }
     }
 
